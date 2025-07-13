48d4636b9ae076733014080264297a209f3c115a
Bhusal-Ravi <132731379+Bhusal-Ravi@users.noreply.github.com>
2025-07-13 20:58:38 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(
 );
 app.use(
     cors({
-    origin:'http://localhost:5175',
+    origin:'https://data-analysis-cjd5.vercel.app',
     methods:"GET,POST,PUT,DELETE",
     credentials:true,
 })
