545d3cb2affccbd8427f883df942e20ddd3d0fe2
Bhusal-Ravi <132731379+Bhusal-Ravi@users.noreply.github.com>
2025-07-13 22:27:20 +0545

    Update passport.js

--- backend/passport.js
+++ backend/passport.js
@@ -7,7 +7,7 @@ passport.use(
         {
             clientID:process.env.CLIENT_ID,
             clientSecret:process.env.CLIENT_SECRET,
-            callbackURL:'/auth/google/callback',
+           callbackURL:process.env.GOOGLE_CALLBACK_URL || 'https://data-analysis-v3pv.onrender.com/auth/google/callback',
             scope:["profile","email"],
         },
         async function(accessToken,refreshToken,profile,done){
